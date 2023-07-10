0d030007cd284a3f4dda574bb9cdd7bb2f8a5e8d
sereno <sereno.project@gmail.com>
2023-07-10 14:08:59 +0330

    Revert "second UI"
    This reverts commit 30b0f6ea9bf2f7d044428a3f33cc7cbd813562dd.

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from tkinter import *
-BACKGROUND_COLOR = "#B1DDC6"
+# This is a sample Python script.
 
-window = Tk()
-window.title("Flash")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-#canvas
-canvas = Canvas(height=500, width=800)
-card_front = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 260, image=card_front)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 260, text="word", font=("Ariel", 40, "italic"))
-canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0, columnspan=2)
-
-#button
-wrong_btn = PhotoImage(file="./images/wrong.png")
-right_btn = PhotoImage(file="./images/right.png")
-
-wrong_button = Button(image=wrong_btn,highlightthickness=0)
-wrong_button.grid(row=1, column=0)
-
-right_button = Button(image=right_btn,highlightthickness=0)
-right_button.grid(row=1, column=1)
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-window.mainloop()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
